e78f3f4decb3d300e7d2719bfe9ce8ba42b46deb
Ernar98s <shernar1298@gmail.com>
2026-03-06 20:09:25 +0500

    feat(host): Event loop page MFF

--- src/pages/EventLoopPage.vue
+++ src/pages/EventLoopPage.vue
@@ -1,6 +1,19 @@
 <template>
-  <section>
-    <h1>Event Loop</h1>
-    <p>Page stub.</p>
-  </section>
+	<section>
+		<h1>Event Loop</h1>
+		<p>Page stub.</p>
+
+		<div ref="container"></div>
+	</section>
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const container = ref();
+
+onMounted(async () => {
+	const remote = await import('http://localhost:5174/src/main.ts');
+	remote.mount(container.value);
+});
+</script>
